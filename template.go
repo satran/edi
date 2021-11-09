def575f45b56b181399dd402bf999ccb2e6ad4db
Satyajit Ranjeev <s@ranjeev.in>
2021-11-09 06:31:11 +0100

    Ensure the objects dir is in PATH

--- template.go
+++ template.go
@@ -11,21 +11,20 @@ import (
 
 type Template struct {
 	root string
-	fns template.FuncMap
+	fns  template.FuncMap
 	*template.Template
 }
 
 func NewTemplate(dir string) *Template {
-	t := Template{root: dir,}
+	t := Template{root: dir}
 	t.fns = template.FuncMap{
 		"title": strings.Title,
-		"link": t.Link,
-		"sh": t.Shell,
+		"link":  t.Link,
+		"sh":    t.Shell,
 	}
 	t.Template = template.New("engine").Funcs(t.fns).Delims("((", "))")
 	return &t
 }
-
 
 func (t *Template) Link(url string, args ...string) string {
 	if len(args) >= 1 {
@@ -34,14 +33,16 @@ func (t *Template) Link(url string, args ...string) string {
 	return fmt.Sprintf(`<a href=%q>%s</a>`, url, url)
 }
 
-
 func (t *Template) Shell(args string) string {
+	objDir := filepath.Join(t.root, "objects")
 	cmd := exec.Command("bash", "-c", args)
+	// todo: this is a simple hack to ensure the scripts in the
+	// object directory is in the PATH.
 	path := os.Getenv("PATH")
-	cmd.Env = append(os.Environ(),
-		fmt.Sprintf("PATH=%s", path),
-	)
-	cmd.Dir = filepath.Join(t.root, "objects")
+	path += ":" + objDir
+	os.Setenv("PATH", path)
+	cmd.Env = append(os.Environ())
+	cmd.Dir = objDir
 	// do nothing as it just shows error code
 	//return err.Error()
 	out, _ := cmd.CombinedOutput()
