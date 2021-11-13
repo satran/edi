97503c21d4100679dac21146c46999cbd3a6abc3
Satyajit Ranjeev <s@ranjeev.in>
2021-11-13 19:50:43 +0100

    Improve the textarea

--- s/edit.js
+++ s/edit.js
@@ -1,9 +1,119 @@
-(function(){
-    // Text area auto grow as described here: https://stackoverflow.com/a/24676492
-    function auto_grow(element) {
-        element.style.height = "5px";
-        element.style.height = (element.scrollHeight)+"px";
+(function() {
+    const editor = document.getElementById("editor");
+    const keymap = {
+        "<": { value: "<>", pos: 1 },
+        "(": { value: "()", pos: 1 },
+        "{": { value: "{}", pos: 1 },
+        "[": { value: "[]", pos: 1 },
+        "'": { value: "''", pos: 1 },
+        '"': { value: '""', pos: 1 },
+        "“": { value: "“”", pos: 1 },
+        "`": { value: "``", pos: 1 },
+        "‘": { value: "‘’", pos: 1 }
+    };
+    const snipmap = {
+        // These make no sense but I'll add them for completeness
+        "1#": "# ",
+        "2#": "## ",
+
+        // These make sense
+        "3#": "### ",
+        "4#": "#### ",
+        "5#": "##### ",
+        "6#": "###### ",
+
+        // Might be a good idea to add a snippet for a tables sometime.
+        "@l": '((link ""))'
+    };
+	  const filename = document.location.pathname;
+
+    function getWord(text, caretPos) {
+        let preText = text.substring(0, caretPos);
+        let split = preText.split(/\s/);
+        return split[split.length - 1].trim();
     }
+
+    function looksLikeBullet(text, caretPos) {
+        let bulletRegex = /^([ \t]*[\*\-\+]\s*).*/gim;
+        let line = text
+            .substring(0, caretPos)
+            .split(/\r?\n|\r/)
+            .pop();
+        let numberedListRegex = /^([ \t]*\d+\.\s*).*/gim;
+        if (bulletRegex.test(line)) {
+            return {
+                bullet: line.replace(bulletRegex, "$1")
+            };
+        } else if (numberedListRegex.test(line)) {
+            return {
+                bullet: line
+                    .replace(numberedListRegex, "$1")
+                    .replace(/\d+/, (number) => +number + 1)
+            };
+        }
+        return false;
+    }
+
+    function resize() {
+        editor.style.height = '24px';
+        editor.style.height = editor.scrollHeight + 'px';
+    }
+
+	  let cancel;
+    editor.addEventListener("keydown", function (event) {
+        if (keymap[event.key]) {
+            event.preventDefault();
+            const pos = editor.selectionStart;
+            editor.value =
+                editor.value.slice(0, pos) +
+                keymap[event.key].value +
+                editor.value.slice(editor.selectionEnd);
+
+            editor.selectionStart = editor.selectionEnd = pos + keymap[event.key].pos;
+        }
+
+        if (event.key === "Tab") {
+            const word = getWord(editor.value, editor.selectionStart);
+            if (word && snipmap[word]) {
+                event.preventDefault();
+                const pos = editor.selectionStart;
+                editor.value =
+                    editor.value.slice(0, pos - word.length) +
+                    snipmap[word] +
+                    editor.value.slice(editor.selectionEnd);
+
+                editor.selectionStart = editor.selectionEnd =
+                    pos + (snipmap[word].length - 1);
+            } else {
+                event.preventDefault();
+                const pos = editor.selectionStart;
+                editor.value =
+                    editor.value.slice(0, pos) +
+                    " " +
+                    editor.value.slice(editor.selectionEnd);
+
+                editor.selectionStart = editor.selectionEnd = pos + 1;
+            }
+        }
+
+        if (event.key === "Enter") {
+            let bullet = looksLikeBullet(editor.value, editor.selectionStart);
+            if (bullet) {
+                event.preventDefault();
+                let addition = editor.value.substring(editor.selectionStart);
+                editor.value = editor.value.substring(0, editor.selectionStart);
+                editor.value += "\n" + bullet.bullet + addition;
+            }
+        }
+        resize();
+
+	      if (cancel) clearTimeout(cancel);
+	      cancel = setTimeout(() => {
+		        localStorage.setItem(filename, event.target.value);
+		    }, 1000);
+    });
+
+    editor.addEventListener('input', resize);
 
     function dropHandler(ev) {
         const fileInput = document.getElementById("file-input");
@@ -46,6 +156,7 @@
         // Prevent default behavior (Prevent file from being opened)
         ev.preventDefault();
     }
+
     const savebtn = document.getElementById("save-btn");
 	  const form = document.getElementById("new-form");
 	  savebtn.addEventListener('click', event => {
@@ -53,14 +164,7 @@
 	      form.submit();
 	  });
 
-	  const editor = document.querySelector("#editor");
-	  auto_grow(editor);
-    editor.addEventListener("input", e => {
-        auto_grow(e.target);
-    });
-
-	  // Save stuff to localstorage
-	  let filename = document.location.pathname;
+	  // Load draft from localstorage
 	  let value = localStorage.getItem(filename);
 	  if (value && value.length > 0) {
 	      if (confirm('Load unsaved draft?')) {
@@ -68,11 +172,6 @@
 	      }
 	  }
 
-	  let cancel;
-	  editor.addEventListener("keyup", event => {
-	      if (cancel) clearTimeout(cancel);
-	      cancel = setTimeout(() => {
-		        localStorage.setItem(filename, event.target.value);
-		    }, 1000);
-	  });
+    // on load for the first time resize the editor to full page
+    resize();
 })();
