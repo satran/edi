8b84e7abe121db66cb44c29f93a8bd5e6dca5fe6
Satyajit Ranjeev <s@ranjeev.in>
2021-11-17 20:55:34 +0100

    Add drag and drop functionality

--- s/edit.js
+++ s/edit.js
@@ -113,47 +113,51 @@
 
     editor.addEventListener('input', resize);
 
-    function dropHandler(ev) {
-        const fileInput = document.getElementById("file-input");
+    const fileEl = document.querySelector('.file');
+    ['dragenter', 'dragover'].forEach(eventName => {
+        fileEl.addEventListener(eventName, highlight, false);
+    });
+
+    ['dragleave', 'drop'].forEach(eventName => {
+        fileEl.addEventListener(eventName, unhighlight, false);
+    });
+
+    function highlight(e) {
+        fileEl.classList.add('highlight');
+    }
+
+    function unhighlight(e) {
+        fileEl.classList.remove('highlight');
+    }
+
+    fileEl.addEventListener("dragstart", ev => {
+        ev.dataTransfer.setData("text",  "data");
+        ev.dataTransfer.effectAllowed = "move";
+    });
+
+    fileEl.addEventListener("drop", ev => {
         console.log('File(s) dropped');
+        ev.preventDefault();
+        let dt = ev.dataTransfer;
+        let files = dt.files;
+        ([...files]).forEach(file => {
+            let formdata = new FormData();
+            formdata.append("name", file.name);
+            formdata.append("file", file);
+            fetch("/_new", {method: "POST", body: formdata})
+                .then(data => {
+                    editor.insertText('((image "'+ file.name +'"))');
+                })
+                .catch((error) => {
+                    console.log("err:", error);
+                });
+        });
+    });
 
+    fileEl.addEventListener("dragover", ev => {
         // Prevent default behavior (Prevent file from being opened)
         ev.preventDefault();
-        let names = "";
-        let dT = new DataTransfer();
-        if (ev.dataTransfer.items) {
-	          // Use DataTransferItemList interface to access the file(s)
-	          for (var i = 0; i < ev.dataTransfer.items.length; i++) {
-	              // If dropped items aren't files, reject them
-	              if (ev.dataTransfer.items[i].kind === 'file') {
-		                var file = ev.dataTransfer.items[i].getAsFile();
-		                console.log('... file[' + i + '].name = ' + file.name);
-		                dT.items.add(file);
-		                names += file.name + ",";
-	              }
-	          }
-        } else {
-	          // Use DataTransfer interface to access the file(s)
-	          for (var i = 0; i < ev.dataTransfer.files.length; i++) {
-	              console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
-	              if (ev.dataTransfer.files[i].kind === 'file') {
-		                var file = ev.dataTransfer.files[i].getAsFile();
-		                console.log('... file[' + i + '].name = ' + file.name);
-		                dT.items.add(file);
-		                names += file.name + ",";
-	              }
-	          }
-        }
-        fileInput.files = dT.files;
-        const nameInput = document.querySelector('input[name="name"]');
-        nameInput.value = names.slice(0, -1);
-        document.querySelector(".editor").hidden = true;
-    }
-
-    function dragOverHandler(ev) {
-        // Prevent default behavior (Prevent file from being opened)
-        ev.preventDefault();
-    }
+    });
 
     const savebtn = document.getElementById("save-btn");
 	  const form = document.getElementById("new-form");
