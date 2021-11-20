484f4f93f42fb31ae4a0faaa92dd3e39462b22c8
Satyajit Ranjeev <s@ranjeev.in>
2021-11-20 12:40:34 +0000

    Remove custom go template

--- parser.go
+++ parser.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"strings"
 	"text/template"
 
 	bf "github.com/russross/blackfriday/v2"
@@ -13,44 +12,20 @@ type Parser struct {
 	root     string
 	fns      template.FuncMap
 	filename string
-	*template.Template
 }
 
 func NewParser(dir string, name string) *Parser {
 	p := Parser{root: dir, filename: name}
-	p.fns = template.FuncMap{
-		"title": strings.Title,
-		"link":  p.Link,
-		"l":     p.Link,
-		"i":     p.Image,
-		"image": p.Image,
-		"sh":    p.Shell,
-		"parse": p.Parse,
-	}
-	p.Template = template.New("engine").Funcs(p.fns).Delims("((", "))")
 	return &p
 }
 
 func (p *Parser) Parse(content string) string {
-	t, err := p.Clone()
-	if err != nil {
-		return fmt.Sprintf("couldn't load parser: %s", err)
-	}
-	t, err = t.Parse(content)
-	if err != nil {
-		return fmt.Sprintf("couldn't parse template: %s", err)
-	}
-	wr := &bytes.Buffer{}
-	if err := t.Execute(wr, nil); err != nil {
-		return fmt.Sprintf("couldn't execute template: %s", err)
-	}
-
 	m := bf.New(bf.WithExtensions(bf.CommonExtensions |
 		bf.HeadingIDs |
 		bf.Footnotes |
 		bf.NoEmptyLineBeforeBlock |
 		bf.AutoHeadingIDs))
-	ast := m.Parse(wr.Bytes())
+	ast := m.Parse([]byte(content))
 	var buf bytes.Buffer
 	renderer := bf.NewHTMLRenderer(bf.HTMLRendererParameters{})
 	ast.Walk(func(node *bf.Node, entering bool) bf.WalkStatus {
